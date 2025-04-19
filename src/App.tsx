ba4cf19eacdde14e9ed9c1c2e98d2932524ab3b5
Dhirendra Singh <127616069+Dksingh566@users.noreply.github.com>
2025-04-19 20:55:31 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Index from './pages/Index';
 import Contact from './pages/Contact';
 import NotFound from './pages/NotFound';
